a22e26df5a0508ea6c34f70dd3d80dcec82dd369
turtle <michael.app.space@gmail.com>
2021-11-18 11:58:02 +0900

    innerHTML to insertAdjacentHTML

--- 2021-11-17/mission1.js
+++ 2021-11-17/mission1.js
@@ -5,15 +5,16 @@ const tToDoListUL = document.querySelector('#todo_list>ul');
 tButton.addEventListener('click', addNewTask);
 
 let idSequence = 0;
-function addNewTask() {
-    tToDoListUL.innerHTML = tToDoListUL.innerHTML +
+function addNewTask(event) {    
+    tToDoListUL.insertAdjacentHTML('beforeend', 
         `<li>
           <input id="${idSequence}" type="checkbox" name="list_item" />
           <label for="${idSequence++}" class="list_content">${tInput.value}</label>          
           <a href="#" onclick="deleteTaskItem(this)"><span class="glyphicon">&#xe020;</span></a>
-        </li>`;
+        </li>`);
 
     tInput.value = '';
+    tInput.focus();
 }
 
 function deleteTaskItem(aTag) {
